0cd432f9ad076f7bc142926950c857d642cbe89f
daizhicheng <daizhicheng@banggood.com>
2017-08-30 14:14:18 +0800

    add web03 by branch1

---
+++ web03/src/main/java/com/dzc/ci/web03/controller/Controller.java
@@ -0,0 +1,16 @@
+package com.dzc.ci.web03.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Administrator on 2017/8/30.
+ */
+@RestController
+public class Controller {
+
+    @GetMapping("/cc")
+    public String cc() {
+        return "hi,cc.";
+    }
+}
